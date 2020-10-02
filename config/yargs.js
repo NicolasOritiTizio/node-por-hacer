4f79acb816e3084175bf4a358c69d42194e34bf8
Nicolas Oriti Tizio <oritinicolas@gmail.com>
2020-10-02 11:30:21 -0300

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+
+const descripcion = {
+    alias: 'd',
+    demand: true,
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+                .command('crear','Crear una tarea por hacer',{
+                    descripcion
+                })
+                .command('actualizar','Actualiza el estado completado de una tarea',{
+                    descripcion,
+                    completado
+                })
+                .command('borrar','borra una tarea por hacer',{
+                    descripcion
+                })
+                .help()
+                .argv;
+
+module.exports = {
+    argv
+}
+
